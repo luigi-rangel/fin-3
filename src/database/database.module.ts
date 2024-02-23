ef887359069160cc76661a551a621fd16b79e8e3
luigi-rangel <luigimsr@dcc.ufrj.br>
2024-02-23 13:23:17 -0300

    expande modelo de dados

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
+@Global()
 @Module({
-  providers: [DatabaseService]
+  providers: [DatabaseService],
+  exports: [DatabaseService]
 })
 export class DatabaseModule {}
